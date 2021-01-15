749be48bb9dcd2ff43978da7525bfdb53c657dc3
Diana-nyamai <dnyamai.dn@gmail.com>
2021-01-15 21:27:52 +0300

    update

--- Cafe.js
+++ Cafe.js
@@ -1,33 +1,33 @@
 import React, { useState } from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Button, Text} from 'react-native';
 
 const Cat = (props) =>{
-  const [isHungry, setIsHungry] = useState(true)
-
-  return(
+    const [isCat, setIsCat] = useState(true);
+return(
     <View>
     <Text>
-    Hello my name is {props.name} , and i am {isHungry? "hungry":"full"}!
-    </Text>
-    <Button 
+    Hello there my name is {props.name} , and i am a {isCat?"CAT":"DOG"}
+
+    <Button
     onPress={() =>{
-      setIsHungry(false);
+        setIsCat(false);
     }}
-    disabled={!isHungry}
-    title={isHungry? "pour me some milk":"no thank you"}
+    disabled={!isCat}
+    title={isCat? "yes i am a cat":"no i am a dog"}
     />
+    </Text>
     </View>
-  )
-}
+)}
+
 
 const Cafe = () =>{
     return(
-        <>
-         <Cat name="daisy"/>
-         <Cat name="elfa"/>
+        <view>
+    <Cat name="daisy"/>
+    <Cat name="elsa"/>
 
-         </>
-    )
-  
+        </view>
+    );
 }
-export default Cafe;
+
+export default Cafe
